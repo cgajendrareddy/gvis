507ce60ce8098224726c486d83c31ad2bff9e39c
hager@linuxuser.at <hager@linuxuser.at@09b82610-ced2-11dd-86d7-074df07e0730>
2008-12-21 18:33:27 +0000

    + GVis.clean() function -- removes all rows and columns, more demo-parameters and data

--- trunk/demos/demodata.js
+++ trunk/demos/demodata.js
@@ -6,13 +6,13 @@ function DefaultData(data, params, source) {
 }
 
 var DefaultDataTypes = {
-    "table": new DefaultData('["CN", 1000], ["US", 5000], ["IN", 2500], ["DE", 4000], ["FR", 2200]', '{ width:600, height:360 }', ''),
+    "table": new DefaultData('["CN", 1000, true], ["US", 5000, false], ["IN", 2500, true], ["DE", 4000, true], ["FR", 2200, true]', '{ width:600, height:360 }', ''),
     "piechart": new DefaultData('["CN", 1000], ["US", 5000], ["IN", 2500], ["DE", 4000], ["FR", 2200]', '{ is3D: true, width:600, height:360 }', ''),
-    "barchart": new DefaultData('["CN", 1000], ["US", 5000], ["IN", 2500], ["DE", 4000], ["FR", 2200]', '{ width:600, height:360 }', ''),
-    "areachart": new DefaultData('["CN", 1000], ["US", 5000], ["IN", 2500], ["DE", 4000], ["FR", 2200]', '{ width:600, height:360 }', ''),
-    "linechart": new DefaultData('["CN", 1000], ["US", 5000], ["IN", 2500], ["DE", 4000], ["FR", 2200]', '{ width:600, height:360 }', ''),
+    "barchart": new DefaultData('["CN", 2000, 1100], ["US", 3500, 2000], ["IN", 2500, 1500], ["DE", 4000, 3000], ["FR", 2200, 1900]', '{ width:600, height:360 }', ''),
+    "areachart": new DefaultData('["CN", 2000, 1100], ["US", 3500, 2000], ["IN", 2500, 1500], ["DE", 4000, 3000], ["FR", 2200, 1900]', '{ width:600, height:360 }', ''),
+    "linechart": new DefaultData('["CN", 2000, 1100], ["US", 3500, 2000], ["IN", 2500, 1500], ["DE", 4000, 3000], ["FR", 2200, 1900]', '{ width:600, height:360 }', ''),
     "scatterchart": new DefaultData('["CN", 1000], ["US", 5000], ["IN", 2500], ["DE", 4000], ["FR", 2200]', '{ width:600, height:360 }', ''),
-    "imagesparkline": new DefaultData('["CN", 1000], ["US", 5000], ["IN", 2500], ["DE", 4000], ["FR", 2200]', '{ width:600, height:360 }', ''),
+    "imagesparkline": new DefaultData('[2000, 1100], [3500, 2000], [2500, 1500], [4000, 3000], [2200, 1900]', '{ width: 320, height: 60, showAxisLines: false,  showValueLabels: false, labelPosition: "left" }', ''),
     "intensitymap": new DefaultData('["CN", 1000], ["US", 5000], ["IN", 2500], ["DE", 4000], ["FR", 2200]', '{ width:600, height:360 }', ''),
     "geomap": new DefaultData('["CN", 1000], ["US", 5000], ["IN", 2500], ["DE", 4000], ["FR", 2200]', '{ width:600, height:360 }', '')
 }
