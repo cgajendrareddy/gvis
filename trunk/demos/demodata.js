7b8a260a3bdf2904df7d6ac0289e8447cf5483bc
hager@linuxuser.at <hager@linuxuser.at@09b82610-ced2-11dd-86d7-074df07e0730>
2008-12-21 20:56:57 +0000

    enhanced demos

--- trunk/demos/demodata.js
+++ trunk/demos/demodata.js
@@ -1,20 +1,21 @@
 // For each map-dype: data + param
-function DefaultData(data, params, source) {
+function DefaultData(data, params, url1, url2) {
     this.data = data;
     this.params = params;
-    this.source = source;
+    this.data_url = url1;
+    this.params_url = url2;
 }
 
 var DefaultDataTypes = {
-    "table": new DefaultData('["CN", 1000, true], ["US", 5000, false], ["IN", 2500, true], ["DE", 4000, true], ["FR", 2200, true]', '{ width:600, height:360 }', ''),
-    "piechart": new DefaultData('["CN", 1000], ["US", 5000], ["IN", 2500], ["DE", 4000], ["FR", 2200]', '{ is3D: true, width:600, height:360 }', ''),
-    "barchart": new DefaultData('["CN", 2000, 1100], ["US", 3500, 2000], ["IN", 2500, 1500], ["DE", 4000, 3000], ["FR", 2200, 1900]', '{ width:600, height:360 }', ''),
-    "areachart": new DefaultData('["CN", 2000, 1100], ["US", 3500, 2000], ["IN", 2500, 1500], ["DE", 4000, 3000], ["FR", 2200, 1900]', '{ width:600, height:360 }', ''),
-    "linechart": new DefaultData('["CN", 2000, 1100], ["US", 3500, 2000], ["IN", 2500, 1500], ["DE", 4000, 3000], ["FR", 2200, 1900]', '{ width:600, height:360 }', ''),
-    "scatterchart": new DefaultData('["CN", 1000], ["US", 5000], ["IN", 2500], ["DE", 4000], ["FR", 2200]', '{ width:600, height:360 }', ''),
-    "imagesparkline": new DefaultData('[2000, 1100], [3500, 2000], [2500, 1500], [4000, 3000], [2200, 1900]', '{ width: 320, height: 60, showAxisLines: false,  showValueLabels: false, labelPosition: "left" }', ''),
-    "intensitymap": new DefaultData('["CN", 1000], ["US", 5000], ["IN", 2500], ["DE", 4000], ["FR", 2200]', '{ width:600, height:360 }', ''),
-    "geomap": new DefaultData('["CN", 1000], ["US", 5000], ["IN", 2500], ["DE", 4000], ["FR", 2200]', '{ width:600, height:360 }', '')
+    "table": new DefaultData('["CN", 1000, true], ["US", 5000, false], ["IN", 2500, true], ["DE", 4000, true], ["FR", 2200, true]', '{ width:400, height:220 }', 'http://code.google.com/apis/visualization/documentation/gallery/table.html#Data_Format', 'http://code.google.com/apis/visualization/documentation/gallery/table.html#Configuration_Options'),
+    "piechart": new DefaultData('["CN", 1000], ["US", 5000], ["IN", 2500], ["DE", 4000], ["FR", 2200]', '{ is3D: true, width:600, height:360 }', 'http://code.google.com/apis/visualization/documentation/gallery/piechart.html#Data_Format', 'http://code.google.com/apis/visualization/documentation/gallery/piechart.html#Configuration_Options'),
+    "barchart": new DefaultData('["CN", 2000, 1100], ["US", 3500, 2000], ["IN", 2500, 1500], ["DE", 4000, 3000], ["FR", 2200, 1900]', '{ width:600, height:360 }', 'http://code.google.com/apis/visualization/documentation/gallery/barchart.html#Data_Format', 'http://code.google.com/apis/visualization/documentation/gallery/barchart.html#Configuration_Options'),
+    "areachart": new DefaultData('["CN", 2000, 1100], ["US", 3500, 2000], ["IN", 2500, 1500], ["DE", 4000, 3000], ["FR", 2200, 1900]', '{ width:600, height:360 }', 'http://code.google.com/apis/visualization/documentation/gallery/areachart.html#Data_Format', 'http://code.google.com/apis/visualization/documentation/gallery/areachart.html#Configuration_Options'),
+    "linechart": new DefaultData('["CN", 2000, 1100], ["US", 3500, 2000], ["IN", 2500, 1500], ["DE", 4000, 3000], ["FR", 2200, 1900]', '{ width:600, height:360 }', 'http://code.google.com/apis/visualization/documentation/gallery/linechart.html#Data_Format', 'http://code.google.com/apis/visualization/documentation/gallery/linechart.html#Configuration_Options'),
+    "scatterchart": new DefaultData('["CN", 1000], ["US", 5000], ["IN", 2500], ["DE", 4000], ["FR", 2200]', '{ width:600, height:360 }', 'http://code.google.com/apis/visualization/documentation/gallery/scatterchart.html#Data_Format', 'http://code.google.com/apis/visualization/documentation/gallery/scatterchart.html#Configuration_Options'),
+    "imagesparkline": new DefaultData('[2000, 1100], [3500, 2000], [2500, 1500], [4000, 3000], [2200, 1900]', '{ width: 320, height: 60, showAxisLines: false,  showValueLabels: false, labelPosition: "left" }', 'http://code.google.com/apis/visualization/documentation/gallery/imagesparkline.html#Data_Format', 'http://code.google.com/apis/visualization/documentation/gallery/imagesparkline.html#Configuration_Options'),
+    "intensitymap": new DefaultData('["CN", 1000], ["US", 5000], ["IN", 2500], ["DE", 4000], ["FR", 2200]', '{ width:600, height:360 }', 'http://code.google.com/apis/visualization/documentation/gallery/intensitymap.html#Data_Format', 'http://code.google.com/apis/visualization/documentation/gallery/intensitymap.html#Configuration_Options'),
+    "geomap": new DefaultData('["CN", 1000], ["US", 5000], ["IN", 2500], ["DE", 4000], ["FR", 2200]', '{ width:600, height:360 }', 'http://code.google.com/apis/visualization/documentation/gallery/geomap.html#Data_Format', 'http://code.google.com/apis/visualization/documentation/gallery/geomap.html#Configuration_Options')
 }
 
 SOURCE_BASE = "";
--- trunk/demos/index.js
+++ trunk/demos/index.js
@@ -7,10 +7,63 @@ var params;
 var data = "";
 var currentType = "geomap";
 var src_orig = "";
+var LASTAUTOVAL = "";
+var COLORS = ["008000"];
+var COLOR_DEFAULT = "3e79ad";
+var COLOR_BUTTONS = 0;
+var CURRENT_COLOR = 0;
 
 function $$(e) { return document.getElementById(e); }
 
-function start() {
+function start() {       
+    $$("id_select_geomap").selected = true;
+ 
+    $("#autocomplete").autocomplete({
+        data: ISOCODES,
+        width:200,
+        height:400        
+    });
+    $("#autocomplete").bind("result", function(e, ui) { 
+        // Reset input field
+        $$("autocomplete").value =LASTAUTOVAL;
+
+        s = String(ui);        
+        short = s.substr(s.indexOf("(")+1, 2);
+        
+        // Check if item is already in list
+        data = $$("id_data").value;
+        preset = "";
+        if (data.indexOf(short) != -1) {
+            preset = data.substr(data.indexOf(short)+4);
+            preset = preset.substr(0, preset.indexOf("]"));
+            while (preset.substr(0, 1) == " ") { preset = preset.substr(1); }
+        }
+        
+        v = prompt("Value for " + ui + ":", preset);
+        if ((v) && (v.length > 0)) {
+            if (preset.length == 0) {
+                $$("id_data").value += ', ["' + short + '", ' + v + ']';
+            } else {
+                new_value = data.substr(0, data.indexOf(short)+4);
+                data = data.substr(new_value.length);
+                data = data.substr(data.indexOf("]"));
+                new_value += " " + v + data;
+                $$("id_data").value = new_value;
+            }                    
+        }
+        
+        refreshData();
+    });
+
+    $("#colorpicker").colorpicker({
+        color: COLORS[0],
+        flat: true,
+        submit: function(e, ui) { 
+            $$("colorpicker").style.display = "none";
+            setColor(CURRENT_COLOR, ui.hex);
+        } 
+    });
+    
     chart = new GVis("chart", currentType);
     src_orig = $$("source").innerHTML;
 
@@ -22,12 +75,16 @@ function start() {
 function updateUI() {
     // Fill Out Textareas with Demo Code
     $$("id_data").value = DefaultDataTypes[currentType].data;
+    $$("id_dataInfo").innerHTML = "<a href='" + DefaultDataTypes[currentType].data_url + "' target='_blank'>data format</a>";
     $$("id_params").value = DefaultDataTypes[currentType].params;
+    $$("id_paramsInfo").innerHTML = "<a href='" + DefaultDataTypes[currentType].params_url + "' target='_blank'>options</a>";
 }
 
 function refreshData() {
     eval("data = [" + $$("id_data").value + "];");
     eval("params = " + $$("id_params").value + ";");
+    
+    chart.clear();
     chart.setRows(data);    
     chart.draw(params);
 
@@ -48,5 +105,82 @@ function changeType(type) {
     updateUI();
 
     chart.setType(type);
+    refreshData();
+}
+
+function colorButton_add() {
+    if ($$("cb" + COLOR_BUTTONS)) {
+        $$("cb" + COLOR_BUTTONS).style.display = "block";    
+    } else {
+        html = "<div class='colorbutton' id='cb" + COLOR_BUTTONS + "' onclick='showColorPicker(" + COLOR_BUTTONS + ")'></div>";
+        $$("colorbuttons").innerHTML += html;
+        if (COLORS[COLOR_BUTTONS]) {
+        } else {
+            COLORS[COLOR_BUTTONS] = COLOR_DEFAULT;
+        }
+        $$("cb" + COLOR_BUTTONS).style.background = "#" + COLORS[COLOR_BUTTONS];
+    }
+    COLOR_BUTTONS += 1;
+    updateColorTag();    
+}
+
+function colorButton_del() {
+    if (COLOR_BUTTONS > 0) {
+        $$("cb" + parseInt(COLOR_BUTTONS-1)).style.display = "none";
+        COLOR_BUTTONS -= 1;
+    }
+    updateColorTag();
+}
+
+function showColorPicker(colorbutton_id) {
+    if (($$("colorpicker").style.display == "block") && (CURRENT_COLOR == colorbutton_id)) {
+        $$("colorpicker").style.display = "none";
+        return ;    
+    }
+    CURRENT_COLOR = colorbutton_id;
+    $("#colorpicker").colorpicker("setColor", COLORS[CURRENT_COLOR]);    
+    $$("colorpicker").style.display = "block";
+}
+
+function setColor(color_id, color) {
+    // After clicking on done on the colorpicker
+    $$("cb" + color_id).style.background = "#" + color;
+    COLORS[color_id] = color;
+    updateColorTag();
+}
+
+function removeColorTag() {
+    v = $$("id_params").value;
+    if (v.indexOf("colors") > -1) {
+        v1 = v.substr(0, v.indexOf("colors"));
+        v2 = v.substr(v.indexOf("colors"));
+        v2 = v2.substr(v2.indexOf("],")+2);
+        while((v2.length>0) && (v2.substr(0,1) == " ")) { v2 = v2.substr(1); }
+        vr = v1 + v2;
+        $$("id_params").value = vr;
+    }
+}
+
+function addColorTag(c) {
+    removeColorTag();
+    v = $$("id_params").value;
+    v = "{ colors:" + c + "," + v.substr(v.indexOf("{")+1);
+    $$("id_params").value = v;
+}
+
+function updateColorTag() {
+    // Update Color HTML Tag
+    if (COLOR_BUTTONS == 0) {
+        // Remove any tag
+        removeColorTag();
+    } else {
+        o = "[";
+        for (var i=0; i<COLOR_BUTTONS; i++) {
+            o += "0x" + COLORS[i] + ", ";
+        }
+        o = o.substr(0, o.length-2) + "]";
+        addColorTag(o);
+    }
+    
     refreshData();
 }
